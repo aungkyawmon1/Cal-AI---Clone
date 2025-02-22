34fa6100c804479b4a79b81b8783b66a4cbd3a93
Aung Kyaw Mon <aungkyawmon.am@gmail.com>
2025-02-22 15:05:23 +0700

    Initial Commit

---
+++ Cal AI - Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cal AI - Clone
+//
+//  Created by Aung Kyaw Mon on 2/22/2568.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
